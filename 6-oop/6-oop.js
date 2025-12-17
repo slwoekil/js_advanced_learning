15302eead0bbe4199fbd4d1468e4a9803e6ff0d8
Artem Sherstnev <artem.sherstnev@x5.ru>
2025-12-17 13:46:36 +0200

    6-oop

--- 6-oop/6-oop.js
+++ 6-oop/6-oop.js
@@ -4,29 +4,52 @@ const Character = function (race, name, language) {
     this.language = language;
 }
 
+const Orc = function (name, weapon) {
+    Character.call(this, 'Орк', name, 'Орочий')
+
+    this.weapon = weapon || 'Кулак';
+};
+
+Orc.prototype = Object.create(Character.prototype);
+Orc.prototype.constructor = Orc;
+
+const Elf = function (name, spellSchool) {
+    Character.call(this, 'Эльф', name, 'Эльфийский');
+
+    this.spell_school = spellSchool || 'Простой';
+}
+
+Elf.prototype = Object.create(Character.prototype);
+Elf.prototype.constructor = Elf;
+
 Character.prototype.say = function () {
     console.log(`Язык: ${this.language}, имя: ${this.name}`);
 }
 
-const orc = new Character('Орк', 'Оркадий', 'Орочий');
-
-orc.say();
-
-orc.weapon = 'Меч';
-
-orc.slam = function () {
+Orc.prototype.slam = function () {
     console.log(`Нанесен удар оружием: ${this.weapon}`);
 }
 
-orc.createSpell = function () {
-    orc.spell = 'Абра-кадабра'
+Elf.prototype.createSpell = function () {
+    this.spell = `Магическая стрела - ${this.spell_school}`
 };
 
-orc.spellCast = function () {
-    console.log(`Применено заклинание ${this.spell}`);
+Elf.prototype.castSpell = function () {
+    console.log(`Применено заклинание: ${this.spell}`);
 }
 
 
-orc.slam();
-orc.createSpell();
-orc.spellCast();
+const character1 = new Character('Человек', 'Локи', 'Человеческий');
+
+character1.say();
+
+const orc1 = new Orc('Злорк');
+
+orc1.say();
+orc1.slam();
+
+const elf1 = new Elf('Виего');
+
+elf1.say();
+elf1.createSpell();
+elf1.castSpell();
